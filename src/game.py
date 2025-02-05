7ccd4ade94ac82b4bde2ceb00e9a3cafc912a370
RGG200 <75140347+RGG200@users.noreply.github.com>
2025-02-05 12:13:28 +0000

    J'ai mis des commentaires

--- src/game.py
+++ src/game.py
@@ -3,7 +3,7 @@ from player import Player
 from events import EventListener
 
 class Game:
-    # Fonction de création de la fenêtre du jeu
+    # Fonction de création de la fenêtre du jeu Résolution : 800x600 (digne d'un jeu des années 2000)
     def __init__(self):
         # Résolution de 800 x 600
         self.screen = pygame.display.set_mode((800, 600))
@@ -12,20 +12,23 @@ class Game:
         self.player = Player(20, 250) # Le joueur
         self.eventlistener = EventListener(self)
 
-    # Evenements
+    # Fonction pour gérer les événements
     def handle_events(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.eventlistener.quit()
-
+    
+    # Raffraichissement de l'écran
     def update(self):
         self.player.update()
 
+    # Fonction de dessin
     def draw(self):
         self.screen.fill((127, 127, 127))
         self.player.draw(self.screen)
         pygame.display.flip()
 
+    # Boucle de jeu principale
     def run(self):
         while self.running:
             self.handle_events()
